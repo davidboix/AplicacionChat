9c738860f609ef3faf4a80f5095b8a7da8c1b69a
203-32\dam <david.boisan.dam@insalfonscostafreda.cat>
2024-04-02 17:55:24 +0200

    Creación del proyecto de la interficie grafica de la aplicación de chat.

---
+++ InterficieChat/src/main/java/exercicis/interficiechat/InterficieChat.java
@@ -0,0 +1,12 @@
+package exercicis.interficiechat;
+
+/**
+ *
+ * @author dam
+ */
+public class InterficieChat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
