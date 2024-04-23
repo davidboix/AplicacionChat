92202280edc0fd85b196bb9d4f4e953a69b49c61
203-32\dam <david.boisan.dam@insalfonscostafreda.cat>
2024-04-23 18:58:27 +0200

    Desenvolupat servidor que rebi un missatge encriptat per part del client, el desencripti i el visualizti per consola, aquest missatge el tornara a encriptar, el servidor tornara a enviar el missatge encriptat al client i que el propi client desencripti el missatge i el mostri per consola, a més a més, hem desenvolupat la funcio que envia la quantitat de clients que estan connectats al servidor al client.

--- InterficieChat/src/main/java/encriptacio/Client2.java
+++ InterficieChat/src/main/java/encriptacio/Client2.java
@@ -3,14 +3,16 @@ package encriptacio;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.Base64;
 import java.util.Scanner;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
- * @author David Boix Sanchez i Oleh Plechiy Tupis Andriyovech
+ * @author David Boix Sanchez
  * @version 1.0
  */
 public class Client2 {
@@ -19,29 +21,49 @@ public class Client2 {
         Scanner lector = new Scanner(System.in);
         final String IPSERVIDOR = "localhost";
         final int PORT = 12345;
-        try {
-            Socket cs = new Socket(IPSERVIDOR, PORT);
+        try ( Socket cs = new Socket(IPSERVIDOR, PORT);
+                DataOutputStream out = new DataOutputStream(cs.getOutputStream());
+                DataInputStream dip = new DataInputStream(cs.getInputStream());) {
 
             KeyGenerator keyGen = KeyGenerator.getInstance("AES");
             SecretKey clau = keyGen.generateKey();
-
-            DataOutputStream out = new DataOutputStream(cs.getOutputStream());
-            DataInputStream dip = new DataInputStream(cs.getInputStream());
-
+            
             byte[] keyBytes = clau.getEncoded();
             out.writeInt(keyBytes.length);
             out.write(keyBytes);
-            System.out.print("Escriu la contrasenya: ");
-            String msg = lector.nextLine();
-            //String msg = "Hola, el meu nom es Oleh";
+
+            //System.out.print("Escriu la contrasenya: ");
+            //String msg = lector.nextLine();
+            String msg = "Hola, el meu nom es Oleh";
             Cipher aesCipher = Cipher.getInstance("AES");
             aesCipher.init(Cipher.ENCRYPT_MODE, clau);
             byte[] msgEncriptat = aesCipher.doFinal(msg.getBytes());
-
+            
             out.writeInt(msgEncriptat.length);
             out.write(msgEncriptat);
 
             System.out.println("Missatge encriptat i clau enviats al servidor...");
+
+            byte[] keyBytesServ = new byte[dip.readInt()];
+            dip.readFully(keyBytesServ);
+
+            SecretKey clauServidor = new SecretKeySpec(keyBytesServ, "AES");
+            //SecretKey clauServidor = new SecretKeySpec(dip.readNBytes(keyBytes.length), "AES");
+
+            byte[] msgEncriptats = new byte[dip.readInt()];
+            dip.readFully(msgEncriptats);
+                        
+            Cipher aesCipher2 = Cipher.getInstance("AES");
+            aesCipher2.init(Cipher.DECRYPT_MODE, clau);
+
+            byte[] msgDesencriptat = aesCipher2.doFinal(msgEncriptats);
+            String missatge = new String(msgDesencriptat);
+//            String base64String = Base64.getEncoder().encodeToString(msgDesencriptat);
+            System.out.println("Quantitat clients connectats: " + missatge);
+            Thread.sleep(5000);
+            dip.close();
+            out.close();
+            cs.close();
 
         } catch (Exception e) {
             e.printStackTrace();
